218e0eba2d8a93ae69e0ccb56952d07501620dd6
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2021-02-25 09:22:20 -0300

    Alteracoes testes para rodar o java

--- Aula01/Prog1/OlaMundo.java
+++ Aula01/Prog1/OlaMundo.java
@@ -1,10 +1,15 @@
 class OlaMundo{
     public static void main(String[] args){
         //Isso é um comentário de uma linha
-        System.out.println(    "Ola Mundo!"    );
+        System.out.println("Ola Mundo!");
+        System.out.println("Quem e Stein?");
+        System.out.println("Windows e melhor que ios");
+        System.out.println("Ctrl + Shit + C abre o cmd mas nao funciona o comando \t ls");
+
         /*
-        * Comentário de diversas linhas
         *
+        * Comentário de diversas linhas
+        * Continuacao dos comentarios
         *
         */
         
